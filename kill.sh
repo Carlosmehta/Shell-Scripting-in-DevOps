e06aebe358d095027800f69ac33522f059108adf
Carlosmehta <bhaskarmehta5657@gmail.com>
2025-05-25 23:47:16 +0530

    kill.sh

---
+++ kill.sh
@@ -0,0 +1,17 @@
+
+Lift your potential.
+verified-v2.svg
+Get Verified by Freelancer.
+Lift your profile to the next level by becoming Verified by Freelancer. The blue badge lets people know that your account is authentic and that you have gone through a process to verify identity that includes a video interview. Get Verified today for ₹7,500.00 INR.
+Verified by Freelancer get instant access to:
+- Projects worth $2,500.00 USD+
+- Hourly projects over $50 USD
+- Winning more jobs, more easily
+Lift your potential.
+verified-v2.svg
+Get Verified by Freelancer.
+Lift your profile to the next level by becoming Verified by Freelancer. The blue badge lets people know that your account is authentic and that you have gone through a process to verify identity that includes a video interview. Get Verified today for ₹7,500.00 INR.
+Verified by Freelancer get instant access to:
+- Projects worth $2,500.00 USD+
+- Hourly projects over $50 USD
+- Winning more jobs, more easily
