cdb33e0f023c5154899993644ec047d86e210043
Carlosmehta <bhaskarmehta5657@gmail.com>
2025-05-28 03:16:04 +0530

    Removed unused scripts from day02

--- bash-lab01/day02/create_user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-read -p "Enter username:" username
-
-echo "you entered $username"
-
--- bash-lab01/day02/hide_pw.sh
+++
@@ -1,5 +0,0 @@
-#1/bi/bash
-
-read -sp "Enter your password: " password
-echo -e  "\nPassword received ✅"
-
