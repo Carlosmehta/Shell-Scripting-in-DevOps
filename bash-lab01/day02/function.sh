cf11d5960cb644b46122e6e15e85de4d67fad173
Carlosmehta <bhaskarmehta5657@gmail.com>
2025-05-28 13:11:55 +0530

    function.sh

---
+++ bash-lab01/day02/function.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#To make function
+
+welcomeNote() {
+
+	echo "-----------------------------"
+	echo "Bhaskar"
+	echo "-----------------------------"
+}
+
+#to call our function
+welcomeNote
+welcomeNote
+welcomeNote
