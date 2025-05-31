ee186635ab31a4b19f429d89767d4622ca0907c1
Carlosmehta <bhaskarmehta5657@gmail.com>
2025-05-31 23:06:23 +0530

    12_arithmetic_ops_with_ui_fps.sh

--- bash-lab01/day01/12_arithmetic_ops_with_ui_fps.sh
+++ bash-lab01/day01/12_arithmetic_ops_with_ui_fps.sh
@@ -2,4 +2,28 @@
 
 # Arthmetic ops Script with User Input and Floating Point Support.
 
+# Maths Calculation with user input
 
+
+# Prompt user
+
+read -p "Enter first number: " x
+read -p "Enter second number: " y
+
+# Integer operations
+multi=$((x * y))
+modulus=$((x % y))
+add=$((x + y))
+subtract=$((x - y))
+
+# Floating point division using 'bc'
+
+division=$(echo "scale=2; $x /$y" | bc)
+
+# Display results
+
+echo "Multiplication: $multi"
+echo "Modulus: $modulus"
+echo "Addition: $add"
+echo "Subtraction: $subtract"
+echo "Division: $division"
