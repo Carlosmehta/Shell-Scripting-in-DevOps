8268f70564d1810e12abbd8ecbc9ba1491f2fd0c
Carlosmehta <bhaskarmehta5657@gmail.com>
2025-06-07 03:32:30 +0530

    02_elif-demo.sh

--- bash-lab01/day02/02_elif-demo.sh
+++ bash-lab01/day02/02_elif-demo.sh
@@ -2,13 +2,20 @@
 
 read -p "Enter your marks: " marks
 
-if [[ $marks -ge 80 ]]
+if  
+	[[ $marks -ge 80 ]]
 then 
 	echo "First Division !!"
 
-elif [[ $marks -ge 60 ]]
+elif 
+	[[ $marks -ge 60 ]]
 then
 	echo "Second Division !!"
+
+elif 
+	[[ $marks -ge 40 ]]
+then
+	echo "Third Division !!"
 else
 	echo "FAIL !!!!"
 
